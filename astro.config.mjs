74de3fc8e14e6e7850d51f4eedeac8f26bb51487
Muhammad Raihan Suryanom <muhammad.suryanom@btpn.com>
2024-07-02 15:38:09 +0700

    feat: setup pwa with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,40 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import pwa from "@vite-pwa/astro";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  adapter: vercel(),
   integrations: [
     react(),
+    pwa({
+      base: "/",
+      scope: "/",
+      workbox: {
+        navigateFallback: "/404",
+        globPatterns: ["**/*.{css,js,html,svg,ico,woff2}"],
+      },
+      includeAssets: ["favicon.svg", "favicon.ico", "lpmq.woff2"],
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Maktabah YARSI",
+        short_name: "Maktabah YARSI",
+        description:
+          "Maktabah YARSI مكتبة يرسي merupakan aplikasi perpustakaan islam digital berbahasa Indonesia yang memungkinkan pengguna untuk mencari topik atau permasalah berdasarkan kata kunci seperti iman, sabar, shalat dan riba.",
+        lang: "id",
+        theme_color: "#10b981",
+      },
+      pwaAssets: {
+        config: true,
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
     tailwind(),
     icon({
       include: {
@@ -16,5 +42,4 @@ export default defineConfig({
       },
     }),
   ],
-  adapter: vercel(),
 });
